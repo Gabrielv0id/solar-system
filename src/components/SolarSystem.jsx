6bddbaf751379fe9454fd999d140ebf6e09c6857
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-10-21 16:27:14 -0300

    exercicio 6 feito

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,21 @@
 import { Component } from 'react';
-// import Planets from '../data/planets';
+import Planets from '../data/planets';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
+    const planetsList = Planets.map((planet) => (<PlanetCard
+      key={ planet.name }
+      planetName={ planet.name }
+      planetImage={ planet.image }
+    />));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          { planetsList }
+        </ul>
       </div>
     );
   }
