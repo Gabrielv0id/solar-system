43c1732326e98fcb9459fb4cff1f051af8a6bd4f
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-10-21 15:41:35 -0300

    exercicio 3 e 4 feitos

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,13 @@
 import { Component } from 'react';
+// import Planets from '../data/planets';
+import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
     );
   }
 }
